96357d38fac8c97b2e0536d9af36e4bd6f0ec641
mini00700 <zhanglan_mini@hotmail.com>
2015-07-07 21:40:43 -0400

    sharethought

--- src/deliverable3_repo/SharethoughtTest.java
+++ src/deliverable3_repo/SharethoughtTest.java
@@ -5,48 +5,40 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class BookmarkTest {
+public class SharethoughtTest {
 
 	static WebDriver driver = new HtmlUnitDriver();
 
 	@Before
 	public void setUp() throws Exception {
-
 		// get into the main page of the web site
 		driver.get("http://halley.exp.sis.pitt.edu/comet/login.do");
 
 		// click the link of sign in and get into the sign in page for next
 		// testing
-		
 
 		driver.findElement(By.name("userEmail")).sendKeys(
 				"lanzhang.lemon@gmail.com");
 		driver.findElement(By.name("password")).sendKeys("lzhang11");
-		WebElement loginButtion = driver.findElement(By.id("btnSignin"));
-		loginButtion.click();
+		WebElement loginButton = driver.findElement(By.id("btnSignin"));
+		loginButton.click();
 	}
 
 	@Test
-	public void bookmarkTest() {
-		// Look for the sign in button (in the login div) and click
-		// to attempt to login
-
-//		WebElement bookmark = driver.findElement(By.id("spanbookcolid8402"));
-		 WebElement bookmark =
-		 driver.findElement(By.cssSelector("a[onclick*='spanbookcolid8402']"));
-		bookmark.click();
-
-		try {
-			String content = bookmark.getText();
-			assertEquals(content, "Unbookmark");
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
+	public void test() {
+		driver.findElement(By.id("txtShare")).sendKeys(
+				"test11");
+		
+		WebElement ShareButton = driver.findElement(By.id("btnShareComment"));
+		ShareButton.click();
+		
+//		find the content of this path:
+		//*[@id="tdRecentActivity"]/table[1]/tbody/tr[2]/td[2]/text()[1]
+		
 	}
 
 }
